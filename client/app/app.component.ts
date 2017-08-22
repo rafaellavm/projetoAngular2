87b71d7d461743f35f7c5b0672b1b7de81952324
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-08-22 10:00:32 -0300

    (01) Bem começado, metade feito (07) O componente principal

---
+++ client/app/app.component.ts
@@ -0,0 +1,11 @@
+//transforma a classe em um componente
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './app/app.component.html'
+})
+
+class AppComponent{
+
+}

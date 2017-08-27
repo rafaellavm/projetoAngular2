823985f85f95ed03e7ceb390c65d686b0265d275
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-08-27 15:20:35 -0300

    (02) Criando mais componentes (01) Novo componente

---
+++ client/app/foto/foto.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'foto',
+    templateUrl:'./foto.component.html'
+})
+
+export class FotoComponent{
+
+}
+
+//<foto></foto>
---
+++ client/app/foto/foto.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {FotoComponent} from './foto.component';
+
+@NgModule({
+    //tudo que o modulo possui
+    declarations: [FotoComponent],
+    exports: [FotoComponent]
+})
+
+export class FotoModule{
+
+}

e8e194109c20ffad65a64f7149c6f2a10290fd48
Rafaela Marraschi <rafaela.vm@gmail.com>
2017-11-02 19:14:42 -0200

    01 - Pipes - 02 - Criando nosso pipe

---
+++ client/app/foto/foto.pipes.ts
@@ -0,0 +1,14 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'filtroPorTitulo'
+})
+
+export class FiltroPorTitulo implements PipeTransform{
+
+    transform(fotos,digitado){
+
+        console.log(fotos);
+        console.log(digitado);
+    }
+}
